fcf5625838fa64d5d1b3c23740325c4077a02f49
GeorgiyPetunin <55709184+GeorgiyPetunin@users.noreply.github.com>
2022-03-23 00:38:15 +0200

    Add files via upload

--- ScalaLab1.scala
+++ ScalaLab1.scala
@@ -1,64 +1,37 @@
 object ScalaLab1{
   def main(args: Array[String]): Unit = {
 
-    println("vector A")
-    val vectorA = FillingSin(3)
-    VectorDisplay(vectorA)
-    println("Vector B")
-    val vectorB = FillingCos(3)
-    VectorDisplay(vectorB)
+    val vectorA = FillingVector(3)
+    VectorDisplay(vectorA, "VectorA")
+    val vectorB = FillingVector(3)
+    VectorDisplay(vectorB, "VectorB")
 
     VectorDisplay(VectorMultiplication(vectorA, vectorB), "Vector multiplication")
-    println("Scalar vector multiplication")
-    println(ScalarMultiplication(vectorA, vectorB))
+    println("Scalar vector multiplication\n" + ScalarMultiplication(vectorA, vectorB))
+    println("Reverse vector\n" + ReverseVector(vectorA))
+
   }
 
-  def VectorDisplay(a : Array[Double]) : Unit = {
-    val resultVector = a.mkString("\n")
-    println(resultVector)
-  }
-
-  def VectorDisplay(a : Array[Double], b : String) : Unit = {
+  def VectorDisplay(a : List[Double], b : String) : Unit = {
     println(b)
-    VectorDisplay(a)
+    println(a.mkString("\n"))
   }
 
-  def FillingSin(size : Int) : Array[Double] = {
-    val a = new Array[Double](size)
-    for(i <- 0 until size){
-      a(i) = Math.sin(i)
-    }
-    a
+  def FillingVector(size : Int) : List[Double] = {
+    List.fill(size){scala.math.sin(util.Random.nextInt())}
   }
 
-  def FillingCos(size : Int) : Array[Double] = {
-    val b = new Array[Double](size)
-    for(i <- 0 until size){
-      b(i) = Math.cos(i)
-    }
-    b
+  def VectorMultiplication(a : List[Double], b : List[Double]) : List[Double] = {
+    a(1)*b(2) - a(2)*b(1) :: a(2)*b.head - a.head*b(2) :: a.head*b(1) - a(1)*b.head :: Nil
   }
 
-  def VectorMultiplication(a : Array[Double], b : Array[Double]) : Array[Double] = {
-    if(a.length != 3 && b.length != 3)
-      throw new IllegalArgumentException
-
-    val result = new Array[Double](3)
-      result(0) = a(1)*b(2) - a(2)*b(1)
-      result(1) = a(2)*b(0) - a(0)*b(2)
-      result(2) = a(0)*b(1) - a(1)*b(0)
-
-    result
+  def ScalarMultiplication(a : List[Double], b : List[Double]) : Double = {
+    a.zip(b).map{
+      case (a, b) => a * b
+    }.sum
   }
 
-  def ScalarMultiplication(a : Array[Double], b : Array[Double]) : Double = {
-    if(a.length != b.length)
-      throw new IllegalArgumentException
-
-    var result : Double = 0
-    for(i <- a.indices)
-      result += a(i) * b(i)
-
-    result
+  def ReverseVector(a : List[Double]) : List[Double] = {
+    a.reverse
   }
 }
